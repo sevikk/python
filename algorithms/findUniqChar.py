19336fcde52bfde1b182c6eba3c6d2d1312693c8
Vsevolod Kuznietsov <vsevolod.kuznietsov@guideline.ai>
2025-01-23 21:23:20 +0200

    find uniq char py

--- algorithms/findUniqChar.py
+++ algorithms/findUniqChar.py
@@ -1,13 +1,22 @@
 def firstUniqChar(str: str):
-    print(type(str), type(str) is not str)
     # if type(str) is not str:
     #     raise ValueError('Value is not string')
     if len(str) == 0:
         raise ValueError('Nothing here')
-    
-    
+
+    collection = dict({})
+    for key in str:
+        collection[key] = (collection.get(key, 0) +
+                           1) if key in collection else 1
+
+    for index in range(len(str)):
+        char = str[index]
+        if collection[char] == 1:
+            return {"char": char, "index": index}
+
+    return -1
 
 
 # firstUniqChar("leetcode")
-firstUniqChar("loveleetcode")
+print(firstUniqChar("loveleetcode"))
 # firstUniqChar("aabb")
--- algorithms/intervals.py
+++ algorithms/intervals.py
@@ -1,8 +1,6 @@
-# Write a function that will take 2 segments as coordinates of their start and end on the plane
-# (e.g. [[[0, 2], [3, 4]], [[10, 20], [-1, 3]]])
-# and return the coordinates of their intersection or, if the segments do not intersect, return 'No intersection'
+# Write a function that will take 2 segments as coordinates of their start and end on the plane (e.g. [[[0, 2], [3, 4]], [[10, 20], [-1, 3]]]) and return the coordinates of their intersection or, if the segments do not intersect, return 'No intersection'
 
-def intervalsIntersection(intervals):
+def intersectionIntervals(intervals):
     intervals_list = intervals[0] + intervals[1]
 
     intervals_list.sort()
@@ -10,20 +8,26 @@ def intervalsIntersection(intervals):
     result = [intervals_list[0]]
 
     for index, key in enumerate(intervals_list):
-        if len(intervals_list) <= index:
-            current = intervals_list[index + 1]
-            previous = result[len(result) - 1]
 
-            if current[0] <= previous[1]:
-                return [previous, current ]
-            else:
-                result = [current]
-    return "No intersection"
+        current = intervals_list[index + 1]
+        previous = result[len(result) - 1]
+
+        if (current[0] <= previous[1]):
+            return [previous, current]
+        else:
+            result = [current]
+            continue
+    return False
 
 
-print(intervalsIntersection([[[0, 0], [3, 3]], [[4, 4], [6, 6]]]))
+try:
+    print(intersectionIntervals([[[0, 0], [3, 3]], [[4, 4], [6, 6]]]))
+except IndexError as e:
+    print("No intersection")
+finally:
+    print("Finally we are here")
 
 
 # [[[0, 2], [3, 4]], [[10, 20], [-1, 3]]]#
 
-#const segment1 = [[[0, 0], [4, 4]], [[4, 4], [6, 6]]]);
+# const segment1 = [[[0, 0], [4, 4]], [[4, 4], [6, 6]]]);
