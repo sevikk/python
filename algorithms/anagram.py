f7a11a06fefd82a32ff55e3303c07a92df8dee41
Vsevolod Kuznietsov <vsevolod.kuznietsov@guideline.ai>
2025-01-24 19:33:52 +0200

    anagram

--- algorithms/anagram.py
+++ algorithms/anagram.py
@@ -19,4 +19,4 @@ def validAnagram(str1, str2):
 # print(validAnagram('awesome', 'awesom')) #// false
 # print(validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana')) #// false
 # print(validAnagram('qwerty', 'qeywrt')) #// true
-# print(validAnagram('texttwisttime', 'Timetwisttext')) #// true
+print(validAnagram('texttwisttime', 'Timetwisttext')) #// true
